b91c7849a9577005e8bc2f13d8b42e634fb6c08b
viethung <viethungdev23@gmail.com>
2023-10-07 07:26:55 +0000

    add new property transaction

--- TsdDelivery.Application/Models/Transaction/Response/TransactionResponse.cs
+++ TsdDelivery.Application/Models/Transaction/Response/TransactionResponse.cs
@@ -7,4 +7,5 @@ public class TransactionResponse
     public string? Status { get; set; }
     public string? PaymentMethod { get; set; }
     public string? Description { get; set; }
+    public DateTime CreationDate { get; set; }
 }
