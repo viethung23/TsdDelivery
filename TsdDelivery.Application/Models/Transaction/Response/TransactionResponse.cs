4f1080db494730065aba5dd89b30dbd1bbd36f2d
viethung <viethungdev23@gmail.com>
2023-10-07 10:42:52 +0000

    fix

--- TsdDelivery.Application/Models/Transaction/Response/TransactionResponse.cs
+++ TsdDelivery.Application/Models/Transaction/Response/TransactionResponse.cs
@@ -1,3 +1,5 @@
+using TsdDelivery.Domain.Entities.Enums;
+
 namespace TsdDelivery.Application.Models.Transaction.Response;
 
 public class TransactionResponse
@@ -7,6 +9,6 @@ public class TransactionResponse
     public string? Status { get; set; }
     public string? PaymentMethod { get; set; }
     public string? Description { get; set; }
-    public string? TransactionType { get; set; }
+    public TransactionType TransactionType { get; set; }
     public DateTime CreationDate { get; set; }
 }
